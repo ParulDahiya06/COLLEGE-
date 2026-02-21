ab95e7890d181191ac6a46d5241f6e2e8bcb772c
ParulDahiya06 <parulchhikara6@gmail.com>
2026-02-22 08:21:19 +1300

    Delete Week 6/Week 6(4).py

--- Week 6/Week 6(4).py
+++
@@ -1,6 +0,0 @@
-# Factorial using lambda (recursive)
-factorial = lambda n: 1 if n == 0 or n == 1 else n * factorial(n - 1)
-
-# Example usage
-num = 10
-print("Factorial of", num, "is:", factorial(num))
